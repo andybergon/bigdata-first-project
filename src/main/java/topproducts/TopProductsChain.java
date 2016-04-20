b989d2a34276926c66d054f43f7872f3e2473ba4
Andrea Bergonzo <andybergon@gmail.com>
2016-04-20 14:53:42 +0200

    fixed first job with 2 map and 2 reduce

--- src/main/java/topproducts/TopProductsChain.java
+++ src/main/java/topproducts/TopProductsChain.java
@@ -45,12 +45,16 @@ public class TopProductsChain extends Configured implements Tool {
 	/* Abbiamo righe in questo formato 2015-8-20,pesce,formaggio,insalata,pane
 	con il primo mapper tagliamo il giorno dalla data e formiamo righe con data,prodotto come
 	chiave e 1 come valore 
+	
+	2015-8-16,pesce,formaggio
+	=>
 	2015-8,pesce,1
 	2015-8,formaggio,1
+	
 	quindi in output abbiamo Text, IntWritable*/
 	public static class Mapper1 extends Mapper<LongWritable, Text, Text, IntWritable> {
 		private Text keyDateProduct = new Text();
-		protected void map(Text key, LongWritable value, Context ctx) throws IOException, InterruptedException {
+		protected void map(LongWritable key, Text value, Context ctx) throws IOException, InterruptedException {
 			String line = value.toString();
 			String[] rowElements = line.split(",");
 			String dateFormatted = rowElements[0].substring(0, 7);
@@ -58,13 +62,16 @@ public class TopProductsChain extends Configured implements Tool {
 				dateFormatted = dateFormatted.substring(0, 6);
 			}
 			for (int i = 1; i < rowElements.length; i++) {
-				keyDateProduct.set(dateFormatted+","+rowElements[i]);
-				ctx.write(key, ONE);
+				keyDateProduct.set(dateFormatted + ":" + rowElements[i]);
+				ctx.write(keyDateProduct, ONE);
 			}
 		}
 	}
-	/* Il primo reducer ci fa le somme quindi 2015-8,pesce,1  2015-8,pesce,1
-	 * 2015-8,pesce,2
+	/* Il primo reducer ci fa le somme quindi 
+	 * 2015-8:pesce 1  
+	 * 2015-8:pesce 1
+	 * =>
+	 * 2015-8:pesce 2
 	 */
 	public static class Reducer1 extends Reducer<Text, IntWritable, Text, IntWritable> {
 		public void reduce(Text key, Iterable<IntWritable> values,
@@ -76,65 +83,117 @@ public class TopProductsChain extends Configured implements Tool {
 			context.write(key, new IntWritable(sum));
 		}
 	}
-	/* Questo secondo mapper deve aggregare per chiave prendendo solamente la data e associando la lista prodotto,quantità
-	 * 2015-8,pesce,2,formaggio,30, ....
+	/* Questo secondo mapper deve splittare la chiave data:prodotto in data: e il valore deve diventare prodotto quantità
+	 * 2015-8:pesce 2, 
+	 * 2015-8:formaggio 30
+	 * => 
+	 * 2015-8:pesce 2
+	 * 2015-8:formaggio 30
+	 * 
 	 * NON so quando prendere i 5 più venduti
 	 */
-	public static class Mapper2 extends Mapper<Text, Text, Text, IntWritable> {
+	public static class Mapper2 extends Mapper<LongWritable, Text, Text, Text> {
 
 		@Override
-		protected void map(Text key, Text value, Context ctx) throws IOException, InterruptedException {
-			ctx.write(key, new IntWritable(Integer.valueOf(value.toString())));
+		protected void map(LongWritable key, Text value, Context ctx) throws IOException, InterruptedException {
+			Text dataKey = new Text();
+			Text prodQuantityValue = new Text();
+			String data, prodQuantity;
+			String[] row = value.toString().split(":");
+			data = row[0];
+			prodQuantity = row[1];
+			
+			dataKey.set(data);
+			prodQuantityValue.set(prodQuantity);
+			
+			ctx.write(dataKey, prodQuantityValue);
+		}
+	}
+	
+	/*
+	 * 2015-8:pesce 2
+	 * 2015-8:formaggio 30
+	 * =>
+	 * 2015-8:pesce 2, formaggio 30 
+	 * 
+	 * */
+	
+	public static class Reducer2 extends Reducer<Text, Text, Text, Text> {
+		@Override
+		public void reduce(Text key, Iterable<Text> values, Context context)
+				throws IOException, InterruptedException {
+			Text monthlyProducts = new Text();
+			String mpString = "";
+			for (Text value : values) {
+				mpString = mpString + value.toString() + ", ";
+			}
+			
+			monthlyProducts.set(mpString);
+			context.write(key, monthlyProducts);
 		}
 	}
 
 	public int run(String[] args) throws Exception {
 		Path input = new Path(args[0]);
-		Path temp1 = new Path("temp");
 		Path output = new Path(args[1]);
+		Path temp = new Path("/tmp");
+		
 		Configuration conf = getConf();
-
+		boolean succ = false;
+		
+		/* JOB 1 */
 		Job job1 = new Job(conf, "top-prod-pass-1");
+		
 		FileInputFormat.addInputPath(job1, input);
-		FileOutputFormat.setOutputPath(job1, temp1);
+//		FileOutputFormat.setOutputPath(job1, output);
+		FileOutputFormat.setOutputPath(job1, temp);
+		
 		job1.setJarByClass(TopProductsChain.class);
+		
 		job1.setMapperClass(Mapper1.class);
 		job1.setCombinerClass(Reducer1.class);
 		job1.setReducerClass(Reducer1.class);
+		
 		job1.setInputFormatClass(TextInputFormat.class);
 		job1.setMapOutputKeyClass(Text.class);
 		job1.setMapOutputValueClass(IntWritable.class);
-		boolean succ = job1.waitForCompletion(true);
+		
+		succ = job1.waitForCompletion(true);
 		if (!succ) {
 			System.out.println("Job1 failed, exiting");
 			return -1;
 		}
 
+		/* JOB 2 */		
 		Job job2 = new Job(conf, "top-prod-pass-2");
-		FileInputFormat.setInputPaths(job2, temp1);
+		
+		FileInputFormat.setInputPaths(job2, temp);
 		FileOutputFormat.setOutputPath(job2, output);
 		job2.setJarByClass(TopProductsChain.class);
+		
 		job2.setMapperClass(Mapper2.class);
-		//job2.setReducerClass(Reducer1.class);
-		job2.setInputFormatClass(KeyValueTextInputFormat.class);
+		job2.setCombinerClass(Reducer2.class);
+		job2.setReducerClass(Reducer2.class);
+		
+		//job2.setInputFormatClass(KeyValueTextInputFormat.class);
+		job2.setInputFormatClass(TextInputFormat.class);
 		job2.setMapOutputKeyClass(Text.class);
-		job2.setMapOutputValueClass(IntWritable.class);
-		job2.setNumReduceTasks(1);
+		job2.setMapOutputValueClass(Text.class);
+		
+		job2.setNumReduceTasks(1); //?
+		
 		succ = job2.waitForCompletion(true);
 		if (!succ) {
 			System.out.println("Job2 failed, exiting");
 			return -1;
 		}
-
+		/*
+		*/
 		return 0;
 
 	}
 
 	public static void main(String[] args) throws Exception {
-		if (args.length != 2) {
-			System.out.println("Usage: TopKRecords /path/to/citation.txt output_dir");
-			System.exit(-1);
-		}
 		int res = ToolRunner.run(new Configuration(), new TopProductsChain(), args);
 		System.exit(res);
 	}
