4f2b8a83332e748ed400393c7b8570ad2e83ecd9
Andrea Bergonzo <andybergon@gmail.com>
2016-04-26 11:09:51 +0200

    added file reading from command
    now use:
    spark-submit \
      --class "spark.topproducts.TopProducts" \
      --master local[4] \
      ~/git/bigdata-primoprogetto/PrimoProgetto-0.0.1-SNAPSHOT.jar \
      /home/andybergon/Desktop/spesa.txt

--- src/main/java/spark/topproducts/TopProducts.java
+++ src/main/java/spark/topproducts/TopProducts.java
@@ -1,35 +1,73 @@
 package spark.topproducts;
 
 import org.apache.spark.api.java.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+
+import scala.Tuple2;
 
 public class TopProducts {
 	public static void main(String[] args) {
 
-		// va a cercare dentro /home/<user>/
-		String logFile = "Desktop/spesa.txt"; // Should be some file on your LOCAL(?) system
+		if (args.length < 1) {
+			System.err.println("Usage: JavaWordCount <file>");
+			System.exit(1);
+		}
 
-		SparkConf conf = new SparkConf().setAppName(TopProducts.class.getSimpleName());
+		String logFile = args[0];
+		SparkConf sparkConf = new SparkConf().setAppName(TopProducts.class.getSimpleName());
+		JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
+		JavaRDD<String> lines = sparkContext.textFile(logFile, 1);
+		//		lines = lines.cache(); // persist RDD with default storage level
 
-		JavaSparkContext sc = new JavaSparkContext(conf);
-
-		JavaRDD<String> logData = sc.textFile(logFile).cache();
-
-		long numAs = logData.filter(new Function<String, Boolean>() {
-			public Boolean call(String s) {
-				return s.contains("a");
+		JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
+			public Iterable<String> call(String s) {
+				return Arrays.asList(Pattern.compile(" ").split(s));
 			}
-		}).count();
+		});
 
-		long numBs = logData.filter(new Function<String, Boolean>() {
-			public Boolean call(String s) {
-				return s.contains("b");
+		JavaPairRDD<String, Integer> ones = words.mapToPair(new PairFunction<String, String, Integer>() {
+			public Tuple2<String, Integer> call(String s) {
+				return new Tuple2<>(s, 1);
 			}
-		}).count();
+		});
 
+		JavaPairRDD<String, Integer> counts = ones.reduceByKey(new Function2<Integer, Integer, Integer>() {
+			public Integer call(Integer i1, Integer i2) {
+				return i1 + i2;
+			}
+		});
+		
+		List<Tuple2<String, Integer>> output = counts.collect();
+		
 		System.out.println("##### RESULTS #####");
-		String output = "Lines with a: " + numAs + ", lines with b: " + numBs;
+		for (Tuple2<?, ?> tuple : output) {
+			System.out.println(tuple._1() + ": " + tuple._2());
+		}
+
+		sparkContext.stop();
+
+		//		long numAs = lines.filter(new Function<String, Boolean>() {
+		//			public Boolean call(String s) {
+		//				return s.contains("a");
+		//			}
+		//		}).count();
+		//
+		//		long numBs = lines.filter(new Function<String, Boolean>() {
+		//			public Boolean call(String s) {
+		//				return s.contains("b");
+		//			}
+		//		}).count();
+
+		//		String output = "Lines with a: " + numAs + ", lines with b: " + numBs;
 		System.out.println(output);
 	}
 }
