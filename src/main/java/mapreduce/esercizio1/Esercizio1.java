2a928c9a6663c487495a1a7b5456e7726db03ce1
Andrea Bergonzo <andybergon@gmail.com>
2016-05-12 00:32:02 +0200

    fix ordering java 7 implementing compatable interface

--- src/main/java/mapreduce/esercizio1/Esercizio1.java
+++ src/main/java/mapreduce/esercizio1/Esercizio1.java
@@ -222,7 +222,7 @@ public class Esercizio1 extends Configured implements Tool {
 		Collections.sort(list, new Comparator<Map.Entry<K, V>>() {
 			@Override
 			public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
-				return (o1.getValue()).compareTo(o2.getValue());
+				return o2.getValue().compareTo(o1.getValue()); // N.B o2 comp o1, not viceversa
 			}
 		});
 
